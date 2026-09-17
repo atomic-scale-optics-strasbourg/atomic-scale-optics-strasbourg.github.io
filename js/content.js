f703d6de45b08256f4dfc1267f9027101fa12702
atomic-scale-optics-strasbourg <gschull@hotmail.com>
2026-09-17 14:54:40 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -261,5 +261,168 @@ const siteContent = {
       hal: "",
       arxiv: ""
     }
+     {
+    year: 2021,
+    title: "Energy funnelling within multichromophore architectures monitored with subnanometre resolution",
+    authors: "S. Cao, A. Rosławska, B. Doppagne, M. Romeo, M. Féron, F. Chérioux, H. Bulou, F. Scheurer, G. Schull",
+    journal: "Nature Chemistry 13, 766 (2021)",
+    journalLink: "https://www.nature.com/articles/s41557-021-00697-z",
+    doi: "10.1038/s41557-021-00697-z"
+  },
+
+  {
+    year: 2020,
+    title: "Single-molecule tautomerization tracking through space- and time-resolved fluorescence spectroscopy",
+    authors: "B. Doppagne, T. Neuman, R. Soria-Martinez, L. E. Parra López, H. Bulou, M. Romeo, S. Berciaud, F. Scheurer, J. Aizpurua, G. Schull",
+    journal: "Nature Nanotechnology 15, 207 (2020)",
+    journalLink: "https://www.nature.com/articles/s41565-019-0620-x",
+    doi: "10.1038/s41565-019-0620-x"
+  },
+
+  {
+    year: 2019,
+    title: "Scanning Tunneling Microscope-Induced Excitonic Luminescence of a Two-Dimensional Semiconductor",
+    authors: "D. Pommier, R. Bretel, L. E. Parra López, F. Fabre, A. Mayne, E. Boer-Duchemin, G. Dujardin, G. Schull, S. Berciaud, E. Le Moal",
+    journal: "Phys. Rev. Lett. 123, 027402 (2019)",
+    journalLink: "https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.123.027402",
+    doi: "10.1103/PhysRevLett.123.027402"
+  },
+
+  {
+    year: 2018,
+    title: "Electrofluorochromism at the single-molecule level",
+    authors: "B. Doppagne, M. C. Chong, H. Bulou, A. Boeglin, F. Scheurer, G. Schull",
+    journal: "Science 361, 251–255 (2018)",
+    journalLink: "https://www.science.org/doi/10.1126/science.aat1603",
+    doi: "10.1126/science.aat1603"
+  },
+  {
+    year: 2018,
+    title: "Fano Description of Single-Hydrocarbon Fluorescence Excited by a Scanning Tunneling Microscope",
+    authors: "J. Kröger, B. Doppagne, F. Scheurer, G. Schull",
+    journal: "Nano Letters 18, 3407–3413 (2018)",
+    journalLink: "https://pubs.acs.org/doi/10.1021/acs.nanolett.8b00304",
+    doi: "10.1021/acs.nanolett.8b00304"
+  },
+  {
+    year: 2018,
+    title: "Bright Electroluminescence from Single Graphene Nanoribbon Junctions",
+    authors: "M. C. Chong, N. Afshar-Imani, F. Scheurer, C. Cardoso, A. Ferretti, D. Prezzi, G. Schull",
+    journal: "Nano Letters 18, 175–181 (2018)",
+    journalLink: "https://pubs.acs.org/doi/10.1021/acs.nanolett.7b03797",
+    doi: "10.1021/acs.nanolett.7b03797"
+  },
+
+  {
+    year: 2017,
+    title: "Conductance and Electroluminescence from On-surface Synthesized Molecular Wires",
+    authors: "M. C. Chong, G. Schull",
+    journal: "Reference Module in Chemistry, Molecular Sciences and Chemical Engineering (2017)"
+  },
+  {
+    year: 2017,
+    title: "Imaging isodensity contours of molecular states with STM",
+    authors: "G. Reecht, B. W. Heinrich, H. Bulou, F. Scheurer, L. Limot, G. Schull",
+    journal: "New Journal of Physics 19, 113033 (2017)",
+    journalLink: "https://iopscience.iop.org/article/10.1088/1367-2630/aa969a",
+    doi: "10.1088/1367-2630/aa969a"
+  },
+  {
+    year: 2017,
+    title: "Le fil moléculaire qui éclaire comme une LED",
+    authors: "F. Scheurer, G. Schull",
+    journal: "La Recherche 552 (2017)"
+  },
+  {
+    year: 2017,
+    title: "Vibronic Spectroscopy with Submolecular Resolution from STM-Induced Electroluminescence",
+    authors: "B. Doppagne, M. C. Chong, E. Lorchat, S. Berciaud, M. Romeo, H. Bulou, A. Boeglin, F. Scheurer, G. Schull",
+    journal: "Phys. Rev. Lett. 118, 127401 (2017)",
+    journalLink: "https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.118.127401",
+    doi: "10.1103/PhysRevLett.118.127401"
+  },
+
+  {
+    year: 2016,
+    title: "Ordinary and Hot Electroluminescence from Single-Molecule Devices: Controlling the Emission Color by Chemical Engineering",
+    authors: "M. C. Chong, L. Sosa-Vargas, H. Bulou, A. Boeglin, F. Scheurer, F. Mathevet, G. Schull",
+    journal: "Nano Letters 16, 6480–6484 (2016)",
+    journalLink: "https://pubs.acs.org/doi/10.1021/acs.nanolett.6b02997",
+    doi: "10.1021/acs.nanolett.6b02997"
+  },
+  {
+    year: 2016,
+    title: "A close-up view of coupled molecules",
+    authors: "G. Schull",
+    journal: "Nature 531, 587 (2016)"
+  },
+  {
+    year: 2016,
+    title: "Launching plasmons with molecules",
+    authors: "G. Schull",
+    journal: "Nature Photonics 10, 208 (2016)"
+  },
+  {
+    year: 2016,
+    title: "Single molecules as whispering galleries for electrons",
+    authors: "G. Reecht, H. Bulou, G. Schull, F. Scheurer",
+    journal: "J. Phys. Condens. Matter 28, 165001 (2016)",
+    journalLink: "https://iopscience.iop.org/article/10.1088/0953-8984/28/16/165001",
+    doi: "10.1088/0953-8984/28/16/165001"
+  },
+  {
+    year: 2016,
+    title: "Narrow-Line Single-Molecule Transducer between Electronic Circuits and Surface Plasmons",
+    authors: "M. C. Chong, G. Reecht, H. Bulou, A. Boeglin, F. Scheurer, F. Mathevet, G. Schull",
+    journal: "Phys. Rev. Lett. 116, 036802 (2016)",
+    journalLink: "https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.116.036802",
+    doi: "10.1103/PhysRevLett.116.036802"
+  },
+
+  {
+    year: 2015,
+    title: "Pulling and Stretching a Molecular Wire to Tune its Conductance",
+    authors: "G. Reecht, H. Bulou, F. Scheurer, V. Speisser, F. Mathevet, C. González, Y. J. Dappe, G. Schull",
+    journal: "J. Phys. Chem. Lett. 6, 2987–2992 (2015)",
+    journalLink: "https://pubs.acs.org/doi/10.1021/acs.jpclett.5b01283",
+    doi: "10.1021/acs.jpclett.5b01283"
+  },
+
+  {
+    year: 2014,
+    title: "Une diode électroluminescente à une molécule",
+    authors: "G. Schull, F. Scheurer, G. Reecht",
+    journal: "Photoniques 72, 42–46 (2014)"
+  },
+  {
+    year: 2014,
+    title: "Emission de lumière sous la pointe d'un microscope à effet tunnel",
+    authors: "G. Schull, E. Boer-Duchemin, G. Comtet, G. Dujardin",
+    journal: "Reflets de la Physique 38, 4 (2014)"
+  },
+  {
+    year: 2014,
+    title: "Structure and dynamics of C60 molecules on Au(111)",
+    authors: "H. Shin, A. Schwarze, R. D. Diehl, K. Pussi, A. Colombier, E. Gaudry, J. Ledieu, G. M. McGuirk, L. N. Serkovic Loli, V. Fournée, L. L. Wang, G. Schull, R. Berndt",
+    journal: "Phys. Rev. B 89, 245428 (2014)",
+    journalLink: "https://journals.aps.org/prb/abstract/10.1103/PhysRevB.89.245428",
+    doi: "10.1103/PhysRevB.89.245428"
+  },
+  {
+    year: 2014,
+    title: "Chemical control of electrical contact to sp2 carbon atoms",
+    authors: "T. Frederiksen, G. Foti, F. Scheurer, V. Speisser, G. Schull",
+    journal: "Nature Communications 5, 3659 (2014)",
+    journalLink: "https://www.nature.com/articles/ncomms4659",
+    doi: "10.1038/ncomms4659"
+  },
+  {
+    year: 2014,
+    title: "Electroluminescence of a Polythiophene Molecular Wire Suspended between a Metallic Surface and the Tip of a Scanning Tunneling Microscope",
+    authors: "G. Reecht, F. Scheurer, V. Speisser, Y. J. Dappe, F. Mathevet, G. Schull",
+    journal: "Phys. Rev. Lett. 112, 047403 (2014)",
+    journalLink: "https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.112.047403",
+    doi: "10.1103/PhysRevLett.112.047403"
+  },
   ]
 };
